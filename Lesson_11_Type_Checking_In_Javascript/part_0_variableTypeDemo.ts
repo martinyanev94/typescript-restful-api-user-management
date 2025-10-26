0be2f6442e2059dbb8a61dedf6346f2a8b3f6659
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 02:12:22 -0400

    Add Lesson_11_Type_Checking_In_Javascript/part_0_variableTypeDemo.ts

---
+++ Lesson_11_Type_Checking_In_Javascript/part_0_variableTypeDemo.ts
@@ -0,0 +1,8 @@
+let variable: any = "Hello, TypeScript!";
+console.log(typeof variable); // Output: string
+
+variable = 42;
+console.log(typeof variable); // Output: number
+
+variable = true;
+console.log(typeof variable); // Output: boolean
