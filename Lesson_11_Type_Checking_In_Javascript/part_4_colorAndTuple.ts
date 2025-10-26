d9177316386af06ba5e0642e8b8e4a0b2817d717
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 02:12:29 -0400

    Add Lesson_11_Type_Checking_In_Javascript/part_4_colorAndTuple.ts

---
+++ Lesson_11_Type_Checking_In_Javascript/part_4_colorAndTuple.ts
@@ -0,0 +1,10 @@
+enum Color {
+    Red,
+    Green,
+    Blue
+}
+
+let favoriteColor: Color = Color.Green;
+console.log(favoriteColor); // Output: 1 (the index of Green)
+let tuple: [string, number] = ["TypeScript", 2023];
+console.log(tuple); // Output: ["TypeScript", 2023]
