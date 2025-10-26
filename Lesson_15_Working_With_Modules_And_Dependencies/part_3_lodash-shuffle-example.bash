be47611c65edba2bb15f9df1b701f6bdc27557b5
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 02:13:00 -0400

    Add Lesson_15_Working_With_Modules_And_Dependencies/part_3_lodash-shuffle-example.bash

---
+++ Lesson_15_Working_With_Modules_And_Dependencies/part_3_lodash-shuffle-example.bash
@@ -0,0 +1,9 @@
+npm install lodash
+import _ from 'lodash';
+
+const numbers = [1, 2, 3, 4, 5];
+const shuffled = _.shuffle(numbers);
+console.log(shuffled); // Outputs a shuffled array
+npm install --save-dev @types/lodash
+npm outdated
+npm update lodash
