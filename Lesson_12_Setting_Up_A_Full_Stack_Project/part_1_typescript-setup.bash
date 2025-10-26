ac1ba44863f3e4c4b677b7b55fa98614fe436c78
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 02:12:33 -0400

    Add Lesson_12_Setting_Up_A_Full_Stack_Project/part_1_typescript-setup.bash

---
+++ Lesson_12_Setting_Up_A_Full_Stack_Project/part_1_typescript-setup.bash
@@ -0,0 +1,12 @@
+npm install typescript ts-node @types/node --save-dev
+npx tsc --init
+{
+  "compilerOptions": {
+    "target": "ES2020",
+    "module": "commonjs",
+    "outDir": "./dist",
+    "rootDir": "./src",
+    "strict": true,
+    "esModuleInterop": true
+  }
+}
