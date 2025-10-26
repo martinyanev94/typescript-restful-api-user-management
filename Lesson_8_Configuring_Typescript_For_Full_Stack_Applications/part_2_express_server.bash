8e4ad4a7207645b8c7f88e22beae2a3fd8d4d1e4
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 02:16:57 -0400

    Add Lesson_8_Configuring_Typescript_For_Full_Stack_Applications/part_2_express_server.bash

---
+++ Lesson_8_Configuring_Typescript_For_Full_Stack_Applications/part_2_express_server.bash
@@ -0,0 +1,13 @@
+mkdir src
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
